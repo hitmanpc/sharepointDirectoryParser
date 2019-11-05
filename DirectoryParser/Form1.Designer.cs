7507ace8891843dae0ee234a661905f56bfadb42
Don Bowman <hitmanpc@gmail.com>
2019-11-05 01:31:57 -0600

    Initial Commit

--- DirectoryParser/Form1.Designer.cs
+++ DirectoryParser/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnDirStart = new System.Windows.Forms.Button();
+            this.textDirStart = new System.Windows.Forms.TextBox();
+            this.btnStartSearch = new System.Windows.Forms.Button();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDirStart
+            // 
+            this.btnDirStart.Location = new System.Drawing.Point(21, 69);
+            this.btnDirStart.Name = "btnDirStart";
+            this.btnDirStart.Size = new System.Drawing.Size(110, 23);
+            this.btnDirStart.TabIndex = 0;
+            this.btnDirStart.Text = "Choose Directory";
+            this.btnDirStart.UseVisualStyleBackColor = true;
+            this.btnDirStart.Click += new System.EventHandler(this.btnDirStart_Click);
+            // 
+            // textDirStart
+            // 
+            this.textDirStart.Location = new System.Drawing.Point(137, 71);
+            this.textDirStart.Name = "textDirStart";
+            this.textDirStart.Size = new System.Drawing.Size(426, 20);
+            this.textDirStart.TabIndex = 1;
+            // 
+            // btnStartSearch
+            // 
+            this.btnStartSearch.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnStartSearch.Location = new System.Drawing.Point(21, 151);
+            this.btnStartSearch.Name = "btnStartSearch";
+            this.btnStartSearch.Size = new System.Drawing.Size(110, 23);
+            this.btnStartSearch.TabIndex = 2;
+            this.btnStartSearch.Text = "Start Search";
+            this.btnStartSearch.UseVisualStyleBackColor = true;
+            this.btnStartSearch.Click += new System.EventHandler(this.btnStartSearch_Click);
+            // 
+            // textOutput
+            // 
+            this.textOutput.Location = new System.Drawing.Point(137, 153);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textOutput.Size = new System.Drawing.Size(625, 274);
+            this.textOutput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.btnStartSearch);
+            this.Controls.Add(this.textDirStart);
+            this.Controls.Add(this.btnDirStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnDirStart;
+        private System.Windows.Forms.TextBox textDirStart;
+        private System.Windows.Forms.Button btnStartSearch;
+        private System.Windows.Forms.TextBox textOutput;
     }
 }
 
--- DirectoryParser/Form1.cs
+++ DirectoryParser/Form1.cs
@@ -1,20 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace DirectoryParser
 {
     public partial class Form1 : Form
     {
+        private static string searchString = @"[!#%&*{}\:<>?\/|""]";
+        private Regex regEx = new Regex(searchString, RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDirStart_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+
+                    textDirStart.Text = fbd.SelectedPath;
+
+                }
+            }
+        }
+
+        private void btnStartSearch_Click(object sender, EventArgs e)
+        {
+            textOutput.Text = "";
+            FindFilesInDirectory(textDirStart.Text);
+            var directories = Directory.GetDirectories(textDirStart.Text, "*.*", SearchOption.AllDirectories);
+            
+            
+            foreach (var directory in directories)
+            {
+                var _directory = directory.Replace(Directory.GetDirectoryRoot(directory), "");
+
+                var match = regEx.Match(_directory);
+                if (match.Success)
+                {
+                    textOutput.AppendText("DirectoryMatch: ");
+                    textOutput.AppendText(directory);
+                    textOutput.AppendText(Environment.NewLine);
+                }
+
+                FindFilesInDirectory(directory);
+            }
+
+        }
+
+        private void FindFilesInDirectory(string directory)
+        {
+            var filesInCurrentDirectory = Directory.GetFiles(directory,"*.*");
+            foreach (var file in filesInCurrentDirectory)
+            {
+                var _file = file.Replace(Directory.GetDirectoryRoot(file), "");
+                var fileMatch = regEx.Match(_file);
+
+                if (fileMatch.Success)
+                {
+                    textOutput.AppendText("FileName Match: ");
+                    textOutput.AppendText(file);
+                    textOutput.AppendText(Environment.NewLine);
+                }
+            }
         }
     }
 }
